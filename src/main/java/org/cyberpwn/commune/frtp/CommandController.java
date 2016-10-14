b680feb047a06c7fe8e095c5bedaa9a0ad5c0509
Daniel Mills <danielmillst@gmail.com>
2016-10-14 03:49:10 -0400

    FRTP remove warmup

--- src/main/java/org/cyberpwn/commune/frtp/CommandController.java
+++ src/main/java/org/cyberpwn/commune/frtp/CommandController.java
@@ -1,14 +1,11 @@
 package org.cyberpwn.commune.frtp;
 
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
-import org.phantomapi.sync.TaskLater;
-import org.phantomapi.util.C;
 
 public class CommandController extends Controller implements CommandExecutor
 {
@@ -17,31 +14,14 @@ public class CommandController extends Controller implements CommandExecutor
 		super(parentController);
 	}
 	
+	@Override
 	public boolean onCommand(CommandSender sender, Command c, String cmd, String[] args)
 	{
 		if(c.getName().equalsIgnoreCase("frtp"))
 		{
 			if(sender instanceof Player)
 			{
-				sender.sendMessage(C.RED + "Wait 3 seconds. Don't move.");
-				Location l = ((Player) sender).getLocation().getBlock().getLocation();
-				
-				new TaskLater(60)
-				{
-					@Override
-					public void run()
-					{
-						if(l.equals(((Player) sender).getLocation().getBlock().getLocation()))
-						{
-							((RTPController) parentController).randomTeleport((Player) sender);
-						}
-						
-						else
-						{
-							sender.sendMessage(C.RED + "You Moved!");
-						}
-					}
-				};
+				((RTPController) parentController).randomTeleport((Player) sender);
 			}
 			
 			return true;
@@ -49,13 +29,13 @@ public class CommandController extends Controller implements CommandExecutor
 		
 		return false;
 	}
-
+	
 	@Override
 	public void onStart()
 	{
 		
 	}
-
+	
 	@Override
 	public void onStop()
 	{
